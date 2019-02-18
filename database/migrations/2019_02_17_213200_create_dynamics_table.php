986bccb876c2bf8d53db5120c09db632f4e71adf
leonelmunozlopez <leonelmunozlopez@gmail.com>
2019-02-17 23:32:01 -0300

    Changes in db and new model

--- database/migrations/2019_02_17_213200_create_dynamics_table.php
+++ database/migrations/2019_02_17_213200_create_dynamics_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateDynamicsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('dynamics', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->boolean('is_admin')->default(false);
-            $table->rememberToken();
+            $table->string('code');
+            $table->string('status')->default('pending');
+            $table->text('description');
+            $table->timestamp('ends_at')->nullable();
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('dynamics');
     }
 }
